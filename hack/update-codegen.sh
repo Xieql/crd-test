86b74b2645a454cf27d0d0beda9a0afa39fff5c2
Xieql <xieqianglong@huawei.com>
2022-11-11 14:43:22 +0800

    fix
    Signed-off-by: Xieql <xieqianglong@huawei.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -9,7 +9,12 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+MODULE=crd-test
+VERSION=v1alpha1
+GROUP=samplecontroller
 
+OUTPUT_PKG=pkg/generated
+APIS_PKG=pkg/apis
 echo "SCRIPT_ROOT ${SCRIPT_ROOT} CODEGEN_PKG ${CODEGEN_PKG}   MODULE ${MODULE}"
 
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,lister,informer" \
@@ -29,9 +34,9 @@ ${GROUP}:${VERSION} \
 
 
 
-
-vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
-  sample-controller/pkg/generated sample-controller/pkg/apis \
-  samplecontroller:v1alpha1 \
-  --output-base "${GOPATH}/src" \
-  --go-header-file "${GOPATH}/src/sample-controller/hack/boilerplate.go.txt"
+#
+#vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
+#  sample-controller/pkg/generated sample-controller/pkg/apis \
+#  samplecontroller:v1alpha1 \
+#  --output-base "${GOPATH}/src" \
+#  --go-header-file "${GOPATH}/src/sample-controller/hack/boilerplate.go.txt"
