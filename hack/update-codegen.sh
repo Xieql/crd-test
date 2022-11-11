647e35d4612e04e0a9390b7c456ec4cad1867114
Xieql <xieqianglong@huawei.com>
2022-11-11 10:59:52 +0800

    fix
    Signed-off-by: Xieql <xieqianglong@huawei.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -24,35 +24,35 @@ export PATH=$PATH:$GOPATH/bin
 echo "Generating with deepcopy-gen"
 deepcopy-gen \
   --go-header-file hack/boilerplate.go.txt \
-  --input-dirs=${APIS_PATH}/policy/v1alpha1 \
-  --output-package=${APIS_PATH}/policy/v1alpha1 \
+  --input-dirs=${APIS_PATH}/samplecontroller/v1alpha1 \
+  --output-package=${APIS_PATH}/samplecontroller/v1alpha1 \
   --output-file-base=zz_generated.deepcopy
 
 echo "Generating with register-gen"
 register-gen \
   --go-header-file hack/boilerplate.go.txt \
-  --input-dirs=${APIS_PATH}/policy/v1alpha1 \
-  --output-package=${APIS_PATH}/policy/v1alpha1 \
+  --input-dirs=${APIS_PATH}/samplecontroller/v1alpha1 \
+  --output-package=${APIS_PATH}/samplecontroller/v1alpha1 \
   --output-file-base=zz_generated.register
 
 echo "Generating with client-gen"
 client-gen \
   --go-header-file hack/boilerplate.go.txt \
   --input-base="" \
-  --input=${APIS_PATH}/policy/v1alpha1 \
+  --input=${APIS_PATH}/samplecontroller/v1alpha1 \
   --output-package=${PKG_PATH}/generated/clientset \
   --clientset-name=versioned
 
 echo "Generating with lister-gen"
 lister-gen \
   --go-header-file hack/boilerplate.go.txt \
-  --input-dirs=${APIS_PATH}/policy/v1alpha1 \
+  --input-dirs=${APIS_PATH}/samplecontroller/v1alpha1 \
   --output-package=${PKG_PATH}/generated/listers
 
 echo "Generating with informer-gen"
 informer-gen \
   --go-header-file hack/boilerplate.go.txt \
-  --input-dirs=${APIS_PATH}/policy/v1alpha1 \
+  --input-dirs=${APIS_PATH}/samplecontroller/v1alpha1 \
   --versioned-clientset-package=${PKG_PATH}/generated/clientset/versioned \
   --listers-package=${PKG_PATH}/generated/listers \
   --output-package=${PKG_PATH}/generated/informers
